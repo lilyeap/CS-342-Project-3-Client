01d75773877b607072179f2d2485d116eee11a83
Emily Phan <ephan3@uic.edu>
2023-11-21 00:01:25 -0600

    new scene after choosing category

--- src/main/java/GuiClient.java
+++ src/main/java/GuiClient.java
@@ -2,6 +2,7 @@
 import java.io.IOException;
 
 import javafx.application.Application;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
@@ -21,14 +22,19 @@ public class GuiClient extends Application{
 //	private TextField ipField = new TextField();
 	private TextField portField = new TextField();
 	private Button loginButton = new Button("Login");
+	private Button b1 = new Button("Animals");
+	private Button b2 = new Button("U.S. States");
+	private Button b3 = new Button("Superheroes");
 
 	private Scene loginScene;
 	private Scene gameScene;
+	private Scene cateScene;
 
 	private TextField categoryField = new TextField();
 	private TextField guessField = new TextField();
 	private Label resultLabel = new Label();
 	private Button submitButton = new Button("Submit Guess");
+	private Stage primaryStage;
 	
 	public static void main(String[] args) {
 		launch(args);
@@ -36,12 +42,14 @@ public class GuiClient extends Application{
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
+		this.primaryStage = primaryStage;
 		primaryStage.setTitle("Word Guessing Game - Client");
 
 		createLoginScene();
+		createCategoryScene();
 		createGameScene();
 
-		primaryStage.setScene(loginScene);
+		primaryStage.setScene(createLoginScene());
 
 		primaryStage.show();
 
@@ -51,7 +59,7 @@ public class GuiClient extends Application{
 				int port = Integer.parseInt(portPass);
 
 				if (connectToServer(port)) {
-					primaryStage.setScene(gameScene);
+					primaryStage.setScene(createCategoryScene());
 				} else {
 					showAlert("Connection Failed", "Unable to connect to the server.");
 				}
@@ -59,11 +67,9 @@ public class GuiClient extends Application{
 				showAlert("Invalid Port", "Please enter a valid port number.");
 			}
 		});
-
-//		submitButton.setOnAction(event -> submitGuess());
 	}
 
-	private void createLoginScene() {
+	private Scene createLoginScene() {
 		Label portLabel = new Label("Please Enter Port Number to Connect to:");
 
 		VBox loginLayout = new VBox(10); // 10 is the spacing between children
@@ -72,21 +78,44 @@ public class GuiClient extends Application{
 				loginButton
 		);
 
-		loginScene = new Scene(loginLayout, 500, 400);
+		return new Scene(loginLayout, 500, 400);
 	}
 
-	private void createGameScene() {
-		Label categoryLabel = new Label("Enter Category:");
-		Label guessLabel = new Label("Enter Guess:");
+	private Scene createCategoryScene() {
+		Label categoryLabel = new Label("Select Category:");
 
-		VBox gameLayout = new VBox();
-		gameLayout.getChildren().addAll(
-				categoryLabel, categoryField,
-				guessLabel, guessField,
-				submitButton, resultLabel
-		);
+		VBox gameLayout = new VBox(10);
+		gameLayout.getChildren().addAll(categoryLabel, b1, b2, b3);
 
-		gameScene = new Scene(gameLayout, 500, 400);
+		gameLayout.setAlignment(Pos.CENTER);
+
+		b1.setOnAction(event -> handleCategoryButtonClick("Animals"));
+		b2.setOnAction(event -> handleCategoryButtonClick("U.S. States"));
+		b3.setOnAction(event -> handleCategoryButtonClick("Superheroes"));
+
+		StackPane centerLayout = new StackPane();
+		centerLayout.getChildren().add(gameLayout);
+		StackPane.setAlignment(gameLayout, Pos.CENTER);
+
+		return new Scene(centerLayout, 500, 400);
+	}
+
+	private Scene createGameScene() {
+		Label promptLabel = new Label("Enter a letter to guess:");
+		TextField letterField = new TextField();
+		Button submitButton = new Button("Submit Guess");
+
+		VBox guessLayout = new VBox(10);
+		guessLayout.getChildren().addAll(promptLabel, letterField, submitButton);
+		guessLayout.setAlignment(Pos.CENTER);
+
+//		submitButton.setOnAction(event -> handleGuessSubmit(letterField.getText()));
+
+		return new Scene(guessLayout, 500, 400);
+	}
+
+	private void handleCategoryButtonClick(String selectedCategory) {
+		primaryStage.setScene(createGameScene());
 	}
 
 	private boolean connectToServer(int port) {
@@ -109,6 +138,7 @@ public class GuiClient extends Application{
 		alert.setContentText(content);
 		alert.showAndWait();
 	}
+
 
 //	private void submitGuess() {
 //		// Implement guess submission logic here
