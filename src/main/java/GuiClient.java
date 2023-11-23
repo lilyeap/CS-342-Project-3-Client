8b6a62e7026336205939e9bedb7a46329f353857
Emily Phan <ephan3@uic.edu>
2023-11-23 16:23:58 -0600

    changed to button layout instead of textfield

--- src/main/java/GuiClient.java
+++ src/main/java/GuiClient.java
@@ -17,13 +17,10 @@ import java.net.Socket;
 
 public class GuiClient extends Application{
 
-//	private Socket socket;
 	DataExchange receiveStatus = new DataExchange("none", '0');
 	private ObjectInputStream inputStream;
 	ListView<String> listItems, listItems2;
 	private ObjectOutputStream outputStream;
-
-//	private TextField ipField = new TextField();
 	private TextField portField = new TextField();
 	private Button loginButton = new Button("Login");
 	private Button b1 = new Button("Animals");
@@ -92,18 +89,6 @@ public class GuiClient extends Application{
 		return new Scene(loginLayout, 500, 400);
 	}
 
-//	private Scene createLoginScene() {
-//		Label portLabel = new Label("Please Enter Port Number to Connect to:");
-//
-//		VBox loginLayout = new VBox(10); // 10 is the spacing between children
-//		loginLayout.getChildren().addAll(
-//				portLabel, portField,
-//				loginButton
-//		);
-//
-//		return new Scene(loginLayout, 500, 400);
-//	}
-
 	private Scene createCategoryScene() {
 		Label categoryLabel = new Label("Select Category:");
 
@@ -123,42 +108,53 @@ public class GuiClient extends Application{
 		return new Scene(centerLayout, 500, 400);
 	}
 
-	private void handleGuessSubmit(char c){
-		DataExchange sendChar = new DataExchange("none", c);
-		sendChar.sendMessage(outputStream);
-
-		receiveStatus = DataExchange.receiveMessage(inputStream);
-        assert receiveStatus != null;
-
-        int remainingGuesses = receiveStatus.getRemainingGuesses();
-		char[] knownLtrs = receiveStatus.getUserGuess();
-		boolean roundEnded = receiveStatus.getRoundEnded();
-
-
-	}
+	private char selectedLetter = '\0'; // Initialize with a default value
 
 	private Scene createGameScene() {
-		Label promptLabel = new Label("Enter a letter to guess:");
-		TextField letterField = new TextField();
+		Button[] letterButtons = createLetterButtons();
 		Button submitButton = new Button("Submit Guess");
 
-		VBox guessLayout = new VBox(10);
-		guessLayout.getChildren().addAll(promptLabel, letterField, submitButton);
-		guessLayout.setAlignment(Pos.CENTER);
+		HBox row1 = new HBox(10, letterButtons[0], letterButtons[1], letterButtons[2], letterButtons[3], letterButtons[4], letterButtons[5], letterButtons[6], letterButtons[7], letterButtons[8], letterButtons[9]);
+		HBox row2 = new HBox(10, letterButtons[10], letterButtons[11], letterButtons[12], letterButtons[13], letterButtons[14], letterButtons[15], letterButtons[16], letterButtons[17], letterButtons[18], letterButtons[19]);
+		HBox row3 = new HBox(10, letterButtons[20], letterButtons[21], letterButtons[22], letterButtons[23], letterButtons[24], letterButtons[25]);
+
+		row1.setAlignment(Pos.CENTER);
+		row2.setAlignment(Pos.CENTER);
+		row3.setAlignment(Pos.CENTER);
+
+		VBox keyboardLayout = new VBox(10, row1, row2, row3, submitButton);
+		keyboardLayout.setAlignment(Pos.BOTTOM_CENTER);
 
 		submitButton.setOnAction(event -> {
-			String text = letterField.getText();
+			submitGuess(String.valueOf(selectedLetter));
 
-			// check if the text is exactly one character
-			if (text.length() == 1) {
-				handleGuessSubmit(text.toLowerCase().charAt(0));
-			} else {
-				showAlert("Submission error", "Please enter a single character.");
+			for (Button button : letterButtons) {
+				if (button.getText().equals(String.valueOf(selectedLetter))) {
+					button.setDisable(true);
+					break;
+				}
 			}
-			letterField.clear();
+			selectedLetter = '\0';
 		});
 
-		return new Scene(guessLayout, 500, 400);
+		return new Scene(keyboardLayout, 500, 400);
+	}
+
+	private Button[] createLetterButtons() {
+		Button[] letterButtons = new Button[26];
+		for (int i = 0; i < 26; i++) {
+			char letter = (char) ('A' + i);
+			letterButtons[i] = new Button(String.valueOf(letter));
+
+			letterButtons[i].setOnAction(event -> {
+				selectedLetter = letter;
+				for (Button button : letterButtons) {
+					button.setStyle("");
+				}
+				((Button) event.getSource()).setStyle("-fx-background-color: lightgray");
+			});
+		}
+		return letterButtons;
 	}
 
 	private void handleCategoryButtonClick(String selectedCategory) {
@@ -179,7 +175,7 @@ public class GuiClient extends Application{
 		});
 
 		client.start();
-		Thread.sleep(500);
+		Thread.sleep(300);
 
 		if (client.isConnected()) {
 			outputStream = client.getOut();
@@ -198,19 +194,39 @@ public class GuiClient extends Application{
 		alert.showAndWait();
 	}
 
+	private void submitGuess(String letter) {
+		try {
+			outputStream.writeObject(letter);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 
-//	private void submitGuess() {
-//		// Implement guess submission logic here
-//		String category = categoryField.getText().toUpperCase();
-//		String guess = guessField.getText().toUpperCase();
-//		String message = "GUESS:" + category + ":" + guess;
+//	private void handleGuessSubmit(char c){
+//		DataExchange sendChar = new DataExchange("none", c);
+//		sendChar.sendMessage(outputStream);
 //
-//		try {
-//			// Send the guess to the server
-//			outputStream.writeObject(message);
-//		} catch (IOException e) {
-//			e.printStackTrace();
-//		}
+//		receiveStatus = DataExchange.receiveMessage(inputStream);
+//		assert receiveStatus != null;
+//
+//		int remainingGuesses = receiveStatus.getRemainingGuesses();
+//		char[] knownLtrs = receiveStatus.getUserGuess();
+//		boolean roundEnded = receiveStatus.getRoundEnded();
+//
+//
 //	}
+	//was in the game scene method
+//	submitButton.setOnAction(event -> {
+//		String text = letterField.getText();
+//
+//		// check if the text is exactly one character
+//		if (text.length() == 1) {
+//			handleGuessSubmit(text.toLowerCase().charAt(0));
+//		} else {
+//			showAlert("Submission error", "Please enter a single character.");
+//		}
+//		letterField.clear();
+//	});
+
 
 }
